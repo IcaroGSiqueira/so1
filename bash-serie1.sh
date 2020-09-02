e6cb2930d85a2cee1e48b7f85552451067d95527
IcaroGSiqueira <icarogs1998@gmail.com>
2020-09-02 16:12:30 -0300

    add lista 1 e 2 - incompletas

---
+++ bash-serie1.sh
@@ -0,0 +1,19 @@
+find -iname solucoes.txt
+
+tail /home/aluno/teste.txt
+
+cp --preserve /home/aluno/pasta1/*.log /home/aluno/pasta2
+
+find teste > /home/aluno/resultados
+
+passwd
+
+ls -l > listagem.txt
+
+top
+
+df -BM
+
+df -h .
+
+grep "#" /home/aluno/teste.txt
---
+++ bash-serie2.sh
@@ -0,0 +1,16 @@
+mv configuração.dat .configuração.dat
+
+ls -l
+
+chmod -R nnn diretorio
+
+chown novo_prop arquivo
+
+rm -i -R diretorio
+
+df -h .
+
+cat meutexto.txt | tr [a-z] [A-Z] > MEUTEXTO.TXT
+
+nano resp1.txt resp2.txt && cat resp1.txt resp2.txt > respostas.txt
+
